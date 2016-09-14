a7048faf2569b2ee85d416a2c40bad315e580550
Michael Hadley <michaelwesthadley@gmail.com>
2016-09-13 21:40:27 -0500

    Add day 07 materials

---
+++ class-files/projects/unity/Day07_Classes/Assets/Scripts/Script01_ClassesDemo.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Debug = ClassUtilities.Debug; // <- Ignore this! For class demos only
+
+public class Script01_ClassesDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Enemy enemy1 = new Enemy("Carl the Goblin", 10);
+		enemy1.Speak();
+		enemy1.SayHealth();
+		enemy1.TakeDamage(5);
+		enemy1.SayHealth();
+		enemy1.TakeDamage(5);
+		enemy1.SayHealth();
+
+		Enemy enemy2 = new Enemy("Radcliff", 20);
+		enemy2.Speak();
+		enemy2.SayHealth();
+		enemy2.TakeDamage(5);
+		enemy2.SayHealth();
+	}
+
+}
+
+public class Enemy {
+
+	// Fields
+	public string Name;
+	private int Health;
+
+	// Constructor
+	public Enemy(string name, int health) {
+		Name = name;
+		Health = health;
+	}
+
+	// Methods
+	public void Speak() {
+		Debug.Log("Hello, I am " + Name + ".");
+	}
+	public void SayHealth() {
+		if (Health > 0) {
+			Debug.Log(Name + ": I have " + Health + " HP.");
+		} else {
+			Debug.Log(Name + ": I am dead.");
+		}
+	}
+	public void TakeDamage(int damage) {
+		Health -= damage;
+	}
+}
---
+++ class-files/projects/unity/Day07_Classes/Assets/Scripts/Script05_ColorLerping.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script05_ColorLerping : MonoBehaviour {
+
+	private Light LightComponent;
+
+	public Color StartColor;
+	public Color EndColor;
+	public float LerpAmount = 0f;
+
+	// Use this for initialization
+	void Start () {
+		LightComponent = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LerpAmount += .25f * Time.deltaTime;
+		LightComponent.color = Color.Lerp(StartColor, EndColor, LerpAmount);
+
+		// Using Time and PingPong to creating a repeating pulse
+		LerpAmount = Mathf.PingPong(Time.time, 4f) / 4f;
+		LightComponent.color = Color.Lerp(StartColor, EndColor, LerpAmount);
+	}
+}
