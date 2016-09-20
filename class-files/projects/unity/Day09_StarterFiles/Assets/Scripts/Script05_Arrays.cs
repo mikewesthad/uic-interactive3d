4de412bd07c37568b918463f84815f47f4f807f8
Michael Hadley <michaelwesthadley@gmail.com>
2016-09-20 15:47:16 -0500

    Add day 09 started files

---
+++ class-files/projects/unity/Day09_StarterFiles/Assets/Scripts/Script05_Arrays.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script05_Arrays : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
