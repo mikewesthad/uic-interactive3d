3c38d4c04fdbe9e64107efd85266c16c6d59e170
Michael Hadley <michaelwesthadley@gmail.com>
2016-09-20 21:39:00 -0500

    Add day 09 solutions

---
+++ class-files/projects/unity/Day09_Class/Assets/Scripts/Script01_Distance.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script01_Distance : MonoBehaviour {
+
+	public Transform PlayerTransform;
+	public float DistanceTrigger = 10f;
+	public Color NearColor;
+	public Color FarColor;
+
+	private MeshRenderer RendererComponent;
+	private Material Mat;
+
+	// Use this for initialization
+	void Start () {
+		RendererComponent = GetComponent<MeshRenderer>();
+		Mat = RendererComponent.material;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Code from the end of last class:
+		float distance = Vector3.Distance(PlayerTransform.position, transform.position);
+		Debug.Log(distance);
+		if (distance <= DistanceTrigger) {
+			// How close are we to the target (as a number from 0 to 1)?
+			float lerpAmount = distance / DistanceTrigger;
+			// 0 when we are right there, 1 when we are DistanceTrigger away
+			Mat.color = Color.Lerp(NearColor, FarColor, lerpAmount);
+		} else {
+			Mat.color = FarColor;
+		}
+	}
+}
---
+++ class-files/projects/unity/Day09_Class/Assets/Scripts/Script02_PoppingLights.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script02_PoppingLights : MonoBehaviour {
+
+	private Rigidbody RigidbodyComponent;
+	private Material Mat;
+	private float NextPopTime;
+	private Light LightComponent;
+
+	// Use this for initialization
+	void Start () {
+		RigidbodyComponent = GetComponent<Rigidbody>();
+		Mat = GetComponent<MeshRenderer>().material;
+		LightComponent = GetComponent<Light>();
+
+		NextPopTime = Time.time + Random.Range(1f, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= NextPopTime) {
+			Pop();
+			NextPopTime = Time.time + Random.Range(1f, 3f);
+		}
+	}
+
+	void OnCollisionEnter() {
+		Color randomColor = Random.ColorHSV(0f, 0.2f, 1f, 1f, 1f, 1f);
+		LightComponent.color = randomColor;
+		Mat.SetColor("_EmissionColor", randomColor);
+	}
+
+	void OnMouseDown() {
+		Pop();
+	}
+
+	void Pop() {
+		// Fire off in a random upward direction
+		Vector3 force = new Vector3(
+			Random.Range(-15f, 15f),
+			Random.Range(15f, 30f),
+			Random.Range(-15f, 15f)
+		);
+		RigidbodyComponent.AddForce(force, ForceMode.Impulse);
+	}
+}
