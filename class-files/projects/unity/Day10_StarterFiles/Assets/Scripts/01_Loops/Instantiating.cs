5b7a6edeeed409b25ae7dfafb1b2986dbd099a0b
Michael Hadley <michaelwesthadley@gmail.com>
2016-09-22 16:02:08 -0500

    Add day 10 starter files

---
+++ class-files/projects/unity/Day10_StarterFiles/Assets/Scripts/01_Loops/Instantiating.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiating : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Creating a copy of an object
+
+		// Spawning and casting
+
+		// Looping and spawning a row
+
+		// Mapping 
+		// Challenge: create a row that forms a rainbow of colors
+
+		// Grid - 2D loop
+
+		// Challenge: create a grid where hue changes along the x-axis and value
+		// changes along the y-axis
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ class-files/projects/unity/Day10_StarterFiles/Assets/Scripts/02_Arrays/Arrays.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour {
+
+	// Array 1: private HighScores that we fill with data in Start
+
+	// Array 2: public PlayerNames that we fill in using the inspector
+
+	// Array 3: array with initial data
+	// Average hours of sunshine per month, data from: 
+	// 	http://www.holiday-weather.com/chicago/averages/
+
+	// Use this for initialization
+	void Start () {
+
+		// Array 1:
+		// Initializing an array
+
+
+		// Array 2:
+		// Looping through an array
+
+
+		// Array 3:
+		// Loop through the array and create an instance of the SimpleCube model
+		// for each month. Manipulate each cube so that:
+		//  - Its height (e.g. y-scale) reflects the hours of sunshine in that 
+		//    month
+		//  - Its color reflects the hours of sunshine in that month (e.g. from
+		//    black to yellow)
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
