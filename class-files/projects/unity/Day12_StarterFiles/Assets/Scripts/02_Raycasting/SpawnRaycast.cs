44b68c2801395ca77ace7c5c912569440df28786
Michael Hadley <michaelwesthadley@gmail.com>
2016-09-29 15:25:53 -0500

    Add day 12

---
+++ class-files/projects/unity/Day12_StarterFiles/Assets/Scripts/02_Raycasting/SpawnRaycast.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Exercise:
+// 	- Instantiate a flower at the hit point (just pick your favorite of the 
+// 	  models)
+//  	- Look at the RaycastHit documentation to figure out how to do that
+// 		- Give the flower a random (uniform) scale in order to add some variety
+//  - Extension: instead of just using your favorite flower model, modify your 
+//    code so that a random flower is planted
+// 		- Remember how we spawned random Pokemon in the last scene?
+//  - Extra bonus: what about animating the flowers so that they scale in from
+//    0 when they are planted?
+// 		- Hint: you can create flower prefabs that have a Grow.cs script 
+// 		  attached. That script can then handle the animation.
+
+
+public class SpawnRaycast : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	 
+	}
+
+}
