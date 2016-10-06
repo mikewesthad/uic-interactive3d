2024c36309f65e71349e45211a6794a87827bbb5
Michael Hadley <michaelwesthadley@gmail.com>
2016-10-06 14:24:54 -0500

    Add starter

---
+++ class-files/projects/unity/Day14_StarterFiles/Day14_StarterFiles/Assets/Scripts/01_Pickup/LogicalOperators.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicalOperators : MonoBehaviour {
+
+	private Camera Cam;
+
+	// Use this for initialization
+	void Start () {
+		Cam = GetComponent<Camera>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// "And" Operator
+
+		// "Or" Operator
+
+		// "Not" operator
+
+		// Combining
+
+	}
+}
